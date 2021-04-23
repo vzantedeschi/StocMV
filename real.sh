5bb066cb7cbe8b7cd5cf778cab420ef16a449f55
Valentina Zantedeschi <vzantedeschi@gmail.com>
2021-04-23 19:14:44 +0200

    try for two values of M

--- real.sh
+++ real.sh
@@ -2,9 +2,13 @@ for b in seeger
 do
 	for d in MNIST PENDIGITS FASHION PROTEIN SENSORLESS SHUTTLE
 	do
-		for r in exact MC FO SO
+		for m in 10 100
 		do
-			python3.6 real.py dataset.name=$d model.M=10 bound.type=$b training.risk=$r num_workers=8
+
+			for r in exact MC FO SO
+			do
+				python3 real.py dataset=$d model.M=$m bound.type=$b training.risk=$r num_workers=8
+			done
 		done
 
 	done
