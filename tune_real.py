04661875f705e78000647600fb08dd0470b68f2b
Valentina Zantedeschi <vzantedeschi@gmail.com>
2021-04-23 15:25:04 +0100

    merge tuning scripts too

--- tune_real.py
+++ tune_real.py
@@ -7,14 +7,14 @@ import torch
 
 from torch.optim import Adam
 from torch.optim.lr_scheduler import ReduceLROnPlateau
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, ConcatDataset
 
 from core.bounds import BOUNDS
-from core.losses import sigmoid_loss
-from core.monitors import MonitorMV
+from core.losses import sigmoid_loss, moment_loss, exp_loss
 from core.utils import deterministic
 from data.datasets import Dataset, TorchDataset
-from models.majority_vote import MajorityVote
+from models.majority_vote import MultipleMajorityVote, MajorityVote
+from models.random_forest import two_forests
 from models.stumps import uniform_decision_stumps
 
 from training_routines import stochastic_routine
@@ -28,7 +28,33 @@ def main(cfg):
 
     print("results will be saved in:", ROOT_DIR.resolve()) 
 
-    data = Dataset(cfg.dataset, normalize=True, data_path=Path(hydra.utils.get_original_cwd()) / "data")
+    # define params for each method
+    risks = { # type: (loss, bound-coeff, distribution-type)
+        "exact": (None, 1., "dirichlet"),
+        "MC": (sigmoid_loss, 1., "dirichlet"),
+        "FO": (lambda x, y, z: moment_loss(x, y, z, order=1), 2., "categorical"),
+        "SO": (lambda x, y, z: moment_loss(x, y, z, order=2), 4., "categorical"),
+        "exp": (lambda x, y, z: exp_loss(x, y, z, c=cfg.training.risk_c), np.exp(cfg.training.risk_c / 2) - 1, "categorical")
+    }
+
+    data = Dataset(cfg.dataset, normalize=True, data_path=Path(hydra.utils.get_original_cwd()) / "data")     
+    
+    m = 0
+    if cfg.model.pred == "stumps-uniform":
+        predictors, M = uniform_decision_stumps(cfg.model.M, data.X_train.shape[1], data.X_train.min(0), data.X_train.max(0))
+
+    elif cfg.model.pred == "rf": # random forest
+
+        if cfg.model.tree_depth == "None":
+            cfg.model.tree_depth = None
+
+        m = cfg.model.m # number of points for learning first prior
+        predictors, M = two_forests(cfg.model.M, cfg.model.m, data.X_train, data.y_train[:, 0], max_samples=cfg.model.bootstrap, max_depth=cfg.model.tree_depth, binary=data.binary)
+
+    else:
+        raise NotImplementedError("model.pred should be one the following: [stumps-uniform, rf]")
+
+    loss, coeff, distr = risks[cfg.training.risk]
 
     bound = None
     if cfg.training.opt_bound:
@@ -37,21 +63,12 @@ def main(cfg):
 
         if cfg.bound.stochastic:
             print("Evaluate bound regularizations over mini-batch")
-            bound = lambda n, m, r: BOUNDS[cfg.bound.type](n, m, r, cfg.bound.delta)
+            bound = lambda n, model, risk: BOUNDS[cfg.bound.type](n, model, risk, delta=cfg.bound.delta, m=int(m*n), coeff=coeff)
 
         else:
             print("Evaluate bound regularizations over whole train+val set")
-            bound = lambda n, m, r: BOUNDS[cfg.bound.type](len(data.X_train) + len(data.X_valid), m, r, cfg.bound.delta)
-
-    loss = None
-    if cfg.training.risk == "MC":
-
-        print("with approximated risk, using sigmoid loss")
-        loss = sigmoid_loss
-
-    deterministic(cfg.training.seed)
-
-    predictors, M = uniform_decision_stumps(cfg.model.M, data.X_train.shape[1], data.X_train.min(0), data.X_train.max(0))
+            n = len(data.X_train) + len(data.X_valid)
+            bound = lambda _, model, risk: BOUNDS[cfg.bound.type](n, model, risk, delta=cfg.bound.delta, m=int(m*n), coeff=coeff)
     
     def objective(trial):
 
@@ -60,21 +77,57 @@ def main(cfg):
         
         print(f"lr={LR}, batch={BATCH}")
 
-        trainloader = DataLoader(TorchDataset(data.X_train, data.y_train), batch_size=BATCH, num_workers=cfg.num_workers, shuffle=True)
-        valloader = DataLoader(TorchDataset(data.X_valid, data.y_valid), batch_size=4096, num_workers=cfg.num_workers, shuffle=False)
+        if cfg.model.pred == "rf": # a loader per posterior
 
-        trainvalloader = DataLoader(TorchDataset(np.vstack([data.X_train, data.X_valid]), np.vstack([data.y_train, data.y_valid])), batch_size=4096, num_workers=cfg.num_workers)
+            m_train = int(len(data.X_train) * cfg.model.m)
+            train1 = TorchDataset(data.X_train[m_train:], data.y_train[m_train:])
+            train2 = TorchDataset(data.X_train[:m_train], data.y_train[:m_train])
+            trainloader = [
+                DataLoader(train1, batch_size=cfg.training.batch_size // 2, num_workers=cfg.num_workers, shuffle=True),
+                DataLoader(train2, batch_size=cfg.training.batch_size // 2, num_workers=cfg.num_workers, shuffle=True)
+            ] 
 
-        beta = torch.ones(M) * cfg.model.prior # prior
+            m_val = int(len(data.X_valid) * cfg.model.m)
+            val1 = TorchDataset(data.X_valid[m_val:], data.y_valid[m_val:])
+            val2 = TorchDataset(data.X_valid[:m_val], data.y_valid[:m_val])
+            valloader = [
+                DataLoader(val1, batch_size=4096, num_workers=cfg.num_workers, shuffle=False),
+                DataLoader(val2, batch_size=4096, num_workers=cfg.num_workers, shuffle=False)
+            ]
+
+            trainvalloader = [
+                DataLoader(ConcatDataset([train1, val1]), batch_size=4096, num_workers=cfg.num_workers),
+                DataLoader(ConcatDataset([train2, val2]), batch_size=4096, num_workers=cfg.num_workers)
+            ]
+
+        else:
+            train = TorchDataset(data.X_train, data.y_train)
+            trainloader = DataLoader(train, batch_size=cfg.training.batch_size, num_workers=cfg.num_workers, shuffle=True)
+
+            val = TorchDataset(data.X_valid, data.y_valid)
+            valloader = DataLoader(val, batch_size=4096, num_workers=cfg.num_workers, shuffle=False)
+
+            trainvalloader = DataLoader(ConcatDataset([train, val]), batch_size=4096, num_workers=cfg.num_workers)
+
+        testloader = DataLoader(TorchDataset(data.X_test, data.y_test), batch_size=4096, num_workers=cfg.num_workers, shuffle=False)
+
+        if cfg.model.pred == "rf":
+            betas = [torch.ones(M) * cfg.model.prior for p in predictors] # prior
+
+            # weights proportional to data sizes
+            model = MultipleMajorityVote(predictors, betas, weights=(cfg.model.m, 1-cfg.model.m), mc_draws=cfg.training.MC_draws, distr=distr)
         
-        model = MajorityVote(predictors, beta, mc_draws=cfg.training.MC_draws)
+        else:
+            betas = torch.ones(M) * cfg.model.prior # prior
+
+            model = MajorityVote(predictors, betas, mc_draws=cfg.training.MC_draws, distr=distr)
 
         optimizer = Adam(model.parameters(), lr=LR)
         # init learning rate scheduler
         lr_scheduler = ReduceLROnPlateau(optimizer, 'min', factor=0.1, patience=2)
         
         try:
-            _, best_val_bound, *_ = stochastic_routine(trainloader, valloader, trainvalloader, None, model, optimizer, lr_scheduler, bound, cfg.bound.type, loss=loss, num_epochs=cfg.training.num_epochs)
+            _, best_val_bound, *_ = stochastic_routine(trainloader, valloader, trainvalloader, None, model, optimizer, bound, cfg.bound.type, loss=loss, num_epochs=cfg.training.num_epochs, lr_scheduler=lr_scheduler)
         except:
             best_val_bound = 1.
 
