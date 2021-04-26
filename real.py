c3dd774c7f35086413e655c264b3c1acaa6fc85c
Valentina Zantedeschi <vzantedeschi@gmail.com>
2021-04-26 09:04:44 +0100

    save intermediate results, in case something goes wrong

--- real.py
+++ real.py
@@ -40,73 +40,6 @@ def main(cfg):
         "exp": (lambda x, y, z: exp_loss(x, y, z, c=cfg.training.risk_c), np.exp(cfg.training.risk_c / 2) - 1, "categorical")
     }
 
-    data = Dataset(cfg.dataset, normalize=True, data_path=Path(hydra.utils.get_original_cwd()) / "data")     
-
-    m = 0
-    if cfg.model.pred == "stumps-uniform":
-        predictors, M = uniform_decision_stumps(cfg.model.M, data.X_train.shape[1], data.X_train.min(0), data.X_train.max(0))
-
-    elif cfg.model.pred == "rf": # random forest
-
-        if cfg.model.tree_depth == "None":
-            cfg.model.tree_depth = None
-
-        m = cfg.model.m # number of points for learning first prior
-        predictors, M = two_forests(cfg.model.M, cfg.model.m, data.X_train, data.y_train[:, 0], max_samples=cfg.model.bootstrap, max_depth=cfg.model.tree_depth, binary=data.binary)
-
-    else:
-        raise NotImplementedError("model.pred should be one the following: [stumps-uniform, rf]")
-
-    loss, coeff, distr = risks[cfg.training.risk]
-
-    bound = None
-    if cfg.training.opt_bound:
-
-        print(f"Optimize {cfg.bound.type} bound")
-
-        if cfg.bound.stochastic:
-            print("Evaluate bound regularizations over mini-batch")
-            bound = lambda n, model, risk: BOUNDS[cfg.bound.type](n, model, risk, delta=cfg.bound.delta, m=int(m*n), coeff=coeff)
-
-        else:
-            print("Evaluate bound regularizations over whole train+val set")
-            n = len(data.X_train) + len(data.X_valid)
-            bound = lambda _, model, risk: BOUNDS[cfg.bound.type](n, model, risk, delta=cfg.bound.delta, m=int(m*n), coeff=coeff)
-
-    if cfg.model.pred == "rf": # a loader per posterior
-
-        m_train = int(len(data.X_train) * cfg.model.m)
-        train1 = TorchDataset(data.X_train[m_train:], data.y_train[m_train:])
-        train2 = TorchDataset(data.X_train[:m_train], data.y_train[:m_train])
-        trainloader = [
-            DataLoader(train1, batch_size=cfg.training.batch_size // 2, num_workers=cfg.num_workers, shuffle=True),
-            DataLoader(train2, batch_size=cfg.training.batch_size // 2, num_workers=cfg.num_workers, shuffle=True)
-        ] 
-
-        m_val = int(len(data.X_valid) * cfg.model.m)
-        val1 = TorchDataset(data.X_valid[m_val:], data.y_valid[m_val:])
-        val2 = TorchDataset(data.X_valid[:m_val], data.y_valid[:m_val])
-        valloader = [
-            DataLoader(val1, batch_size=4096, num_workers=cfg.num_workers, shuffle=False),
-            DataLoader(val2, batch_size=4096, num_workers=cfg.num_workers, shuffle=False)
-        ]
-
-        trainvalloader = [
-            DataLoader(ConcatDataset([train1, val1]), batch_size=4096, num_workers=cfg.num_workers),
-            DataLoader(ConcatDataset([train2, val2]), batch_size=4096, num_workers=cfg.num_workers)
-        ]
-
-    else:
-        train = TorchDataset(data.X_train, data.y_train)
-        trainloader = DataLoader(train, batch_size=cfg.training.batch_size, num_workers=cfg.num_workers, shuffle=True)
-
-        val = TorchDataset(data.X_valid, data.y_valid)
-        valloader = DataLoader(val, batch_size=4096, num_workers=cfg.num_workers, shuffle=False)
-
-        trainvalloader = DataLoader(ConcatDataset([train, val]), batch_size=4096, num_workers=cfg.num_workers)
-
-    testloader = DataLoader(TorchDataset(data.X_test, data.y_test), batch_size=4096, num_workers=cfg.num_workers, shuffle=False)
-
     train_errors, test_errors, train_losses, bounds, times = [], [], [], [], []
     for i in range(cfg.num_trials):
         
@@ -114,6 +47,85 @@ def main(cfg):
 
         SAVE_DIR = ROOT_DIR / f"seed={cfg.training.seed+i}"
         SAVE_DIR.mkdir(parents=True, exist_ok=True)
+
+        if (SAVE_DIR / "err-b.npy").is_file():
+            # load save stats
+            seed_results = np.load(SAVE_DIR / "err-b.npy", allow_pickle=True).item()
+
+            train_errors.append(seed_results["train-error"])
+            test_errors.append(seed_results["test-error"])
+            bounds.append(seed_results[cfg.bound.type])
+            times.append(seed_results["time"])
+            train_losses.append(seed_results.pop("train-risk", None)) # available only for non-exact methods
+
+            continue
+
+        data = Dataset(cfg.dataset, normalize=True, data_path=Path(hydra.utils.get_original_cwd()) / "data")     
+
+        m = 0
+        if cfg.model.pred == "stumps-uniform":
+            predictors, M = uniform_decision_stumps(cfg.model.M, data.X_train.shape[1], data.X_train.min(0), data.X_train.max(0))
+
+        elif cfg.model.pred == "rf": # random forest
+
+            if cfg.model.tree_depth == "None":
+                cfg.model.tree_depth = None
+
+            m = cfg.model.m # number of points for learning first prior
+            predictors, M = two_forests(cfg.model.M, cfg.model.m, data.X_train, data.y_train[:, 0], max_samples=cfg.model.bootstrap, max_depth=cfg.model.tree_depth, binary=data.binary)
+
+        else:
+            raise NotImplementedError("model.pred should be one the following: [stumps-uniform, rf]")
+
+        loss, coeff, distr = risks[cfg.training.risk]
+
+        bound = None
+        if cfg.training.opt_bound:
+
+            print(f"Optimize {cfg.bound.type} bound")
+
+            if cfg.bound.stochastic:
+                print("Evaluate bound regularizations over mini-batch")
+                bound = lambda n, model, risk: BOUNDS[cfg.bound.type](n, model, risk, delta=cfg.bound.delta, m=int(m*n), coeff=coeff)
+
+            else:
+                print("Evaluate bound regularizations over whole train+val set")
+                n = len(data.X_train) + len(data.X_valid)
+                bound = lambda _, model, risk: BOUNDS[cfg.bound.type](n, model, risk, delta=cfg.bound.delta, m=int(m*n), coeff=coeff)
+
+        if cfg.model.pred == "rf": # a loader per posterior
+
+            m_train = int(len(data.X_train) * cfg.model.m)
+            train1 = TorchDataset(data.X_train[m_train:], data.y_train[m_train:])
+            train2 = TorchDataset(data.X_train[:m_train], data.y_train[:m_train])
+            trainloader = [
+                DataLoader(train1, batch_size=cfg.training.batch_size // 2, num_workers=cfg.num_workers, shuffle=True),
+                DataLoader(train2, batch_size=cfg.training.batch_size // 2, num_workers=cfg.num_workers, shuffle=True)
+            ] 
+
+            m_val = int(len(data.X_valid) * cfg.model.m)
+            val1 = TorchDataset(data.X_valid[m_val:], data.y_valid[m_val:])
+            val2 = TorchDataset(data.X_valid[:m_val], data.y_valid[:m_val])
+            valloader = [
+                DataLoader(val1, batch_size=4096, num_workers=cfg.num_workers, shuffle=False),
+                DataLoader(val2, batch_size=4096, num_workers=cfg.num_workers, shuffle=False)
+            ]
+
+            trainvalloader = [
+                DataLoader(ConcatDataset([train1, val1]), batch_size=4096, num_workers=cfg.num_workers),
+                DataLoader(ConcatDataset([train2, val2]), batch_size=4096, num_workers=cfg.num_workers)
+            ]
+
+        else:
+            train = TorchDataset(data.X_train, data.y_train)
+            trainloader = DataLoader(train, batch_size=cfg.training.batch_size, num_workers=cfg.num_workers, shuffle=True)
+
+            val = TorchDataset(data.X_valid, data.y_valid)
+            valloader = DataLoader(val, batch_size=4096, num_workers=cfg.num_workers, shuffle=False)
+
+            trainvalloader = DataLoader(ConcatDataset([train, val]), batch_size=4096, num_workers=cfg.num_workers)
+
+        testloader = DataLoader(TorchDataset(data.X_test, data.y_test), batch_size=4096, num_workers=cfg.num_workers, shuffle=False)
 
         if cfg.model.pred == "rf":
             betas = [torch.ones(M) * cfg.model.prior for p in predictors] # prior
@@ -131,11 +143,13 @@ def main(cfg):
         # init learning rate scheduler
         lr_scheduler = ReduceLROnPlateau(optimizer, 'min', factor=0.1, patience=2)
 
+        seed_results = {}
         if cfg.training.risk in ["exact", "MC"]:
             
             *_, best_train_stats, test_error, time = stochastic_routine(trainloader, valloader, trainvalloader, testloader, model, optimizer, bound, cfg.bound.type, loss=loss, monitor=monitor, num_epochs=cfg.training.num_epochs, lr_scheduler=lr_scheduler)
             
             train_errors.append(best_train_stats['error'])
+            seed_results["train-error"] = best_train_stats['error']
 
         else:
             *_, best_train_stats, test_error, train_error, time = stochastic_routine(trainloader, valloader, trainvalloader, testloader, model, optimizer, bound, cfg.bound.type, loss=loss, loss_eval=loss, monitor=monitor, num_epochs=cfg.training.num_epochs, lr_scheduler=lr_scheduler)
@@ -143,9 +157,19 @@ def main(cfg):
             train_errors.append(train_error['error'])
             train_losses.append(best_train_stats['error'])
 
+            seed_results["train-error"] = train_error['error']
+            seed_results["train-risk"] = best_train_stats['error']
+
         test_errors.append(test_error['error'])
         bounds.append(best_train_stats[cfg.bound.type])
         times.append(time)
+
+        seed_results["test_error"] = test_error['error']
+        seed_results[cfg.bound.type] = best_train_stats[cfg.bound.type]
+        seed_results["time"] = time
+
+        # save seed results
+        np.save(SAVE_DIR / "err-b.npy", seed_results)
 
         monitor.close()
 
