found bug in randomized MV baseline: forgot to consider N factor in KL

models/majority_vote.py
@@ -5,7 +5,7 @@ from core.distributions import distr_dict
 
 class MajorityVote(torch.nn.Module):
 
-    def __init__(self, voters, prior, mc_draws=10, distr="dirichlet"):
+    def __init__(self, voters, prior, mc_draws=10, distr="dirichlet", kl_factor=1.):
 
         super(MajorityVote, self).__init__()
         
@@ -22,6 +22,7 @@ class MajorityVote(torch.nn.Module):
         post = torch.rand(self.num_voters) * 2 + 1e-9 # uniform draws in (0, 2]
         self.post = torch.nn.Parameter(torch.log(post), requires_grad=True)  # use log (and apply exp(post) later so that posterior parameters are always positive)
         self.distribution = distr_dict[distr](self.post, mc_draws)
+        self.kl_factor = kl_factor
 
     def forward(self, x):
         return self.voters(x)
@@ -67,7 +68,7 @@ class MajorityVote(torch.nn.Module):
 
     def KL(self):
 
-        return self.distribution.KL(self.prior)
+        return self.kl_factor * self.distribution.KL(self.prior)
 
     def get_post(self):
         return torch.exp(self.post)
@@ -89,7 +90,7 @@ class MajorityVote(torch.nn.Module):
 
 class MultipleMajorityVote(torch.nn.Module):
 
-    def __init__(self, voter_sets, priors, weights, mc_draws=10, posteriors=None, distr="dirichlet"):
+    def __init__(self, voter_sets, priors, weights, mc_draws=10, posteriors=None, distr="dirichlet",  kl_factor=1.):
 
         super(MultipleMajorityVote, self).__init__()
 
@@ -99,10 +100,10 @@ class MultipleMajorityVote(torch.nn.Module):
         if posteriors is not None:
             assert len(priors) == len(posteriors), "must specify same number of priors and posteriors"
 
-            self.mvs = torch.nn.ModuleList([MajorityVote(voters, prior, mc_draws=mc_draws, posterior=post, distr=distr) for voters, prior, post in zip(voter_sets, priors, posteriors)])
+            self.mvs = torch.nn.ModuleList([MajorityVote(voters, prior, mc_draws=mc_draws, posterior=post, distr=distr, kl_factor=kl_factor) for voters, prior, post in zip(voter_sets, priors, posteriors)])
 
         else:
-            self.mvs = torch.nn.ModuleList([MajorityVote(voters, prior, mc_draws=mc_draws, distr=distr) for voters, prior in zip(voter_sets, priors)])
+            self.mvs = torch.nn.ModuleList([MajorityVote(voters, prior, mc_draws=mc_draws, distr=distr, kl_factor=kl_factor) for voters, prior in zip(voter_sets, priors)])
 
         self.weights = weights
 

toy.py
@@ -34,13 +34,13 @@ def main(cfg):
     print("results will be saved in:", SAVE_DIR.resolve()) 
     
     # define params for each method
-    risks = { # type: (loss, bound-coeff, distribution-type)
-        "exact": (None, 1., "dirichlet"),
-        "MC": (sigmoid_loss, 1., "dirichlet"),
-        "rand": (lambda x, y, z: rand_loss(x, y, z, n=cfg.training.rand_n), 2., "categorical"),
-        "FO": (lambda x, y, z: moment_loss(x, y, z, order=1), 2., "categorical"),
-        "SO": (lambda x, y, z: moment_loss(x, y, z, order=2), 4., "categorical"),
-        "exp": (lambda x, y, z: exp_loss(x, y, z, c=cfg.training.exp_c), np.exp(cfg.training.exp_c / 2) - 1, "categorical")
+    risks = { # type: (loss, bound-coeff, distribution-type, kl factor)
+        "exact": (None, 1., "dirichlet", 1.),
+        "MC": (sigmoid_loss, 1., "dirichlet", 1.),
+        "Rnd": (lambda x, y, z: rand_loss(x, y, z, n=cfg.training.rand_n), 2., "categorical", cfg.training.rand_n),
+        "FO": (lambda x, y, z: moment_loss(x, y, z, order=1), 2., "categorical", 1.),
+        "SO": (lambda x, y, z: moment_loss(x, y, z, order=2), 4., "categorical", 1.),
+        "exp": (lambda x, y, z: exp_loss(x, y, z, c=cfg.training.exp_c), np.exp(cfg.training.exp_c / 2) - 1, "categorical", 1.)
     }
 
     train_errors, test_errors, train_losses, bounds, times = [], [], [], [], []
@@ -70,18 +70,18 @@ def main(cfg):
 
         monitor = MonitorMV(SAVE_DIR)
 
-        loss, coeff, distr = risks[cfg.training.risk]
+        loss, coeff, distr, kl_factor = risks[cfg.training.risk]
 
         if cfg.model.pred == "rf":
             betas = [torch.ones(M) * cfg.model.prior for p in predictors] # prior
 
             # weights proportional to data sizes
-            model = MultipleMajorityVote(predictors, betas, weights=(cfg.model.m, 1-cfg.model.m), mc_draws=cfg.training.MC_draws, distr=distr)
+            model = MultipleMajorityVote(predictors, betas, weights=(cfg.model.m, 1-cfg.model.m), mc_draws=cfg.training.MC_draws, distr=distr, kl_factor=kl_factor)
         
         else:
             betas = torch.ones(M) * cfg.model.prior # prior
 
-            model = MajorityVote(predictors, betas, mc_draws=cfg.training.MC_draws, distr=distr)
+            model = MajorityVote(predictors, betas, mc_draws=cfg.training.MC_draws, distr=distr, kl_factor=kl_factor)
 
         # get voter predictions
         if cfg.model.pred == "rf":